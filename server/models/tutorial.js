9c2ac45f27a5019acd5739c44eeb6721423c408f
shanxi85 <shanxi_gill85@yahoo.com>
2020-09-23 09:00:02 +0800

    tutorial

--- server/models/tutorial.js
+++ server/models/tutorial.js
@@ -7,13 +7,3 @@ module.exports = (sequelize, DataTypes) => {
 
 	return Tutorial;
 };
-
-
-//const Tutorial = sequelize.define('Tutorial', {
-//	title: DataTypes.STRING,
-//	description: DataTypes.STRING,
-//});
-
-//const Tag = sequelize.define('Tag', {
-//  name: DataTypes.STRING,
-//});
